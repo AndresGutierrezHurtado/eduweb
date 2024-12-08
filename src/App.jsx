7cd3eb646fad08eaa8a59d405caa7645ad618411
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-12-08 16:37:10 -0500

    feat: add middlewares

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,33 @@ import AppLayout from "./layouts/appLayout";
 
 // Contexts
 import { AuthContextProvider } from "./contexts/authContext";
+import ProtectedRoutes from "./middlewares/protectedRoutes";
 
 export default function App() {
     return (
         <BrowserRouter>
             <AuthContextProvider>
                 <Routes>
-                    <Route path="/" element={<AppLayout />}>
-                        <Route path="/" element={<Home />} />
+                    {/* Rutas con autenticacion requerida */}
+                    <Route element={<ProtectedRoutes mustBeAuth />}>
+                        <Route element={<AppLayout />}>
+                            <Route path="/profile" element={<h2>Perfil</h2>} />
+                        </Route>
                     </Route>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/recovery" element={<Recovery />} />
-                    <Route path="/reset/:token" element={<Reset />} />
+
+                    {/* Rutas publicas */}
+                    <Route element={<AppLayout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/courses" element={<h2>Cursos</h2>} />
+                    </Route>
+
+                    {/* Rutas sin autenticacion requerida */}
+                    <Route element={<ProtectedRoutes mustBeGuest />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/recovery" element={<Recovery />} />
+                        <Route path="/reset/:token" element={<Reset />} />
+                    </Route>
                 </Routes>
             </AuthContextProvider>
         </BrowserRouter>
