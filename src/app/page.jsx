17edb9fda1e8eb6b2b4d442dad72abaa6cead019
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-04-02 12:02:26 -0500

    feat: add new sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 
 // Components
-import { ArrowRight, CalendarIcon, CertificateIcon, ClockIcon, UserIcon } from "@/components/icons";
+import {
+    ArrowRight,
+    CalendarIcon,
+    CertificateIcon,
+    ClockIcon,
+    QuotesIcon,
+    UserIcon,
+} from "@/components/icons";
 
 export default async function Home() {
     return (
-        <>
+        <div className="flex flex-col gap-[100px]">
             <section className="w-full px-3">
-                <div className="w-full max-w-[1200px] mx-auto py-10">
-                    <div className="flex items-center lg:py-14 mb-15gt">
+                <div className="w-full max-w-[1200px] mx-auto py-[100px]">
+                    <div className="flex items-center">
                         <div className="w-full flex flex-col gap-5">
                             <div>
                                 <h3 className="uppercase text-xl font-bold text-primary">
@@ -56,51 +63,122 @@ export default async function Home() {
                             <div className="flex flex-col gap-5 items-center w-full">
                                 <UserIcon size={70} />
                                 <div className="flex flex-col gap-1">
-
-                                <h3 className="text-xl font-bold">Expertos en Latam</h3>
-                                <p className="text-sm text-pretty text-base-content/80">
-                                    Tu equipo aprenderá de profesionales
-                                    reconocidos de Latinoamérica y España.
-                                </p>
+                                    <h3 className="text-xl font-bold">
+                                        Expertos en Latam
+                                    </h3>
+                                    <p className="text-sm text-pretty text-base-content/80">
+                                        Tu equipo aprenderá de profesionales
+                                        reconocidos de Latinoamérica y España.
+                                    </p>
                                 </div>
                             </div>
                             <div className="flex flex-col gap-5 items-center w-full">
                                 <ClockIcon size={70} />
                                 <div className="flex flex-col gap-1">
-
-                                <h3 className="text-xl font-bold">Expertos en Latam</h3>
-                                <p className="text-sm text-pretty text-base-content/80">
-                                    Tu equipo aprenderá de profesionales
-                                    reconocidos de Latinoamérica y España.
-                                </p>
+                                    <h3 className="text-xl font-bold">
+                                        Expertos en Latam
+                                    </h3>
+                                    <p className="text-sm text-pretty text-base-content/80">
+                                        Tu equipo aprenderá de profesionales
+                                        reconocidos de Latinoamérica y España.
+                                    </p>
                                 </div>
                             </div>
                             <div className="flex flex-col gap-5 items-center w-full">
                                 <CalendarIcon size={70} />
                                 <div className="flex flex-col gap-1">
-
-                                <h3 className="text-xl font-bold">Expertos en Latam</h3>
-                                <p className="text-sm text-pretty text-base-content/80">
-                                    Tu equipo aprenderá de profesionales
-                                    reconocidos de Latinoamérica y España.
-                                </p>
+                                    <h3 className="text-xl font-bold">
+                                        Expertos en Latam
+                                    </h3>
+                                    <p className="text-sm text-pretty text-base-content/80">
+                                        Tu equipo aprenderá de profesionales
+                                        reconocidos de Latinoamérica y España.
+                                    </p>
                                 </div>
                             </div>
                             <div className="flex flex-col gap-5 items-center w-full">
                                 <CertificateIcon size={70} />
                                 <div className="flex flex-col gap-1">
-
-                                <h3 className="text-xl font-bold">Expertos en Latam</h3>
-                                <p className="text-sm text-pretty text-base-content/80">
-                                    Tu equipo aprenderá de profesionales
-                                    reconocidos de Latinoamérica y España.
-                                </p>
+                                    <h3 className="text-xl font-bold">
+                                        Expertos en Latam
+                                    </h3>
+                                    <p className="text-sm text-pretty text-base-content/80">
+                                        Tu equipo aprenderá de profesionales
+                                        reconocidos de Latinoamérica y España.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-        </>
+            <section className="w-full px-3">
+                <div className="w-full max-w-[1200px] mx-auto py-10">
+                    <div className="flex items-center gap-10">
+                        <div className="w-full flex flex-col gap-5">
+                            <h2 className="text-5xl font-extrabold">
+                                Obten un <br />
+                                <span className="text-primary italic">
+                                    Certificado oficial
+                                </span>
+                                <br /> de EduWeb
+                            </h2>
+                            <p className="text-lg leading-tight  text-pretty text-base-content/80">
+                                Demuestra tus habilidades con certificaciones
+                                reconocidas por la industria a nivel
+                                internacional.
+                            </p>
+                        </div>
+                        <div className="w-full flex items-center justify-center">
+                            <Image
+                                src={"/certificate.png"}
+                                width={500}
+                                height={500}
+                                alt="Certificado"
+                                className="object-contain rounded border-2 border-white"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="w-full px-3">
+                <div className="w-full max-w-[1200px] mx-auto py-10">
+                    <div className="flex flex-col gap-9">
+                        <h2 className="text-3xl font-extrabold text-center">
+                            Mira lo que están haciendo nuestros estudiantes
+                        </h2>
+                        <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-10">
+                            <div className="bg-base-content/20 p-8 rounded-lg flex flex-col gap-5">
+                                <QuotesIcon size={40} />
+                                <p className="text-sm italic">
+                                    Udemy marcó un antes y un después en mi vida
+                                    y me fue de gran ayuda a la hora de hacer
+                                    realidad la idea de Dimensional.
+                                </p>
+                                <div className="flex gap-3 ">
+                                    <div className="avatar">
+                                        <div className="w-15 aspect-square rounded-full">
+                                            <img
+                                                src="https://cms-images.udemycdn.com/96883mtakkm8/1Djz6c0gZLaCG5SQS3PgUY/54b6fb8c85d8da01da95cbb94fa6335f/Alvin_Lim.jpeg"
+                                                alt="Imagen testimonio"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-col gap-1 text-sm">
+                                        <p className="font-semibold">
+                                            Alvin Lim
+                                        </p>
+                                        <p className="text-base-content/70">
+                                            Cofundador técnico y director de
+                                            tecnología en Dimensional
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 }
