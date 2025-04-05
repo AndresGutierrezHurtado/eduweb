d568a589e42be311dedb4c8f6c94446042633ac9
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-04-05 16:37:56 -0500

    feat: add new users api route

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -30,3 +30,35 @@ export async function GET(request, { params }) {
         );
     }
 }
+
+export async function PUT(request, { params }) {
+    try {
+        const { id } = await params;
+        const { user: userJSON } = await request.json();
+
+        const user = await User.findByPk(id, {
+            include: ["role"],
+        });
+
+        const updatedUser = await user.update(userJSON);
+
+        return NextResponse.json(
+            {
+                success: true,
+                data: updatedUser,
+                message: "Usuarios actualizados correctamente",
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            {
+                success: false,
+                data: error,
+                message: "Error al obtener los usuarios: " + error.message,
+            },
+            { status: 500 }
+        );
+    }
+}
