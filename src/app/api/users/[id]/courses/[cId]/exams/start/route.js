0b3403fe7c6cae28b0ece881e09f9a969f554c04
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-04-06 03:22:22 -0500

    feat: add userExam routes

---
+++ src/app/api/users/[id]/courses/[cId]/exams/start/route.js
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+
+import { UserCourse, UserExam } from "@/database/models";
+
+export async function GET(req, { params }) {
+    try {
+        const { id, cId } = await params;
+
+        const userCourse = await UserCourse.findOne({
+            where: {
+                user_id: id,
+                course_id: cId,
+            },
+        });
+
+        const userExams = await UserExam.findAll({
+            where: {
+                user_course_id: userCourse.user_course_id,
+            },
+        });
+
+        let recentExam = userExams.find((exam) => {
+            const createdAt = new Date(exam.createdAt);
+            const now = new Date();
+            const diff = now.getTime() - createdAt.getTime();
+            return diff <= 10 * 60 * 1000;
+        });
+
+        if (!recentExam) {
+            recentExam = await UserExam.create({
+                user_course_id: userCourse.user_course_id,
+            });
+        }
+
+        return NextResponse.json(
+            {
+                success: true,
+                data: recentExam,
+                message: "Examen obtenido correctamente",
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            {
+                success: false,
+                data: error,
+                message: "Error al obtener el examen",
+            },
+            { status: 500 }
+        );
+    }
+}
