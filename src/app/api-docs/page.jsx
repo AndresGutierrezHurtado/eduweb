4f8fa5855cb2b0b31d9aecbcc31e47805905f4e0
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-04-07 23:23:50 -0500

    fix: fix swagger docs

--- src/app/api-docs/page.jsx
+++ src/app/api-docs/page.jsx
@@ -1,16 +1,19 @@
-'use client';
+"use client";
 
-import React from 'react';
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-import swaggerDocument from '@/lib/swagger.json';
+import React, { useEffect } from "react";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+import swaggerDocument from "@/lib/swagger.json";
 
 const ApiDocs = () => {
-  return (
-    <div className="min-h-screen bg-white p-4">
-      <SwaggerUI spec={swaggerDocument} />
-    </div>
-  );
+    useEffect(() => {
+        document.title = "Documentación de la API | EduWeb";
+    }, []);
+    return (
+        <div className="min-h-screen bg-white p-4">
+            <SwaggerUI spec={swaggerDocument} />
+        </div>
+    );
 };
 
-export default ApiDocs; 
+export default ApiDocs;
